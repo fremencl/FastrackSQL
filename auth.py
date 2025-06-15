a1e509bff54fad8f5a2762e1bca4308d2d8a64b2
Cristian <128819507+fremencl@users.noreply.github.com>
2025-06-15 17:53:36 -0400

    Update auth.py
    Migracion Google Cloud

--- auth.py
+++ auth.py
@@ -1,6 +1,7 @@
 # auth.py
 import hmac
 import streamlit as st
+import os
 
 def check_password():
     """
@@ -8,22 +9,16 @@ def check_password():
     Caso contrario, muestra un cuadro de texto para ingresar password y retorna `False`.
     """
     def password_entered():
-        """
-        Verifica si la contraseña ingresada por el usuario coincide con la de Streamlit Secrets.
-        Si coincide, setea una variable de sesión para no volver a pedirla en la misma sesión.
-        Si no coincide, marca la variable de sesión como incorrecta.
-        """
-        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+        app_password = os.getenv("PASSWORD")  # Leer desde variable de entorno
+        if hmac.compare_digest(st.session_state["password"], app_password):
             st.session_state["password_correct"] = True
             del st.session_state["password"]  # Se elimina la contraseña de la sesión por seguridad.
         else:
             st.session_state["password_correct"] = False
 
-    # Si el usuario ya validó correctamente la contraseña en esta sesión, retornamos True.
     if st.session_state.get("password_correct", False):
         return True
 
-    # De lo contrario, mostramos el campo para ingresar la contraseña.
     st.text_input(
         "Ingrese la contraseña",
         type="password",
@@ -31,9 +26,7 @@ def check_password():
         key="password"
     )
 
-    # Si se validó como incorrecta, mostramos un mensaje de error.
     if "password_correct" in st.session_state and st.session_state["password_correct"] is False:
         st.error("❌ Contraseña incorrecta. Intente de nuevo.")
 
-    # Retornamos False para indicar que no se ha validado o no coincide.
     return False
